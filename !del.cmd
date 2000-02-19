ebb64e45b9caffe0a8e30525e5e5dfd2ac8f50f5
Markus Kämmerer <mk@openxp.de>
2000-02-19 11:46:16 +0000

    Meine Batchfiles brauchen nicht von CVS verwaltet werden

--- !del.cmd
+++
@@ -1,2 +0,0 @@
-@echo off 
-del *.~* *.bak *.fpd *.sym *.tpu *.wup *.res *.db1 *.ix1 *.xps *.eb1 *.dq *.dat *.dof *.exe *.o *.ppu *.a ppas.bat *.ow *.ppw *.dcu *. *.oo2 *.ppo *.sw *.dif *.old xpoint*.cfg
