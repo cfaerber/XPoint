2bec25d248b8c8e61ee30dfd949d505a83e09d5e
Markus Kämmerer <mk@openxp.de>
2000-04-20 16:18:21 +0000

    JG: Fix fuer Anzeigeproblem in Datumsbezuege aktualisieren

--- xp7o.pas
+++ xp7o.pas
@@ -399,7 +399,10 @@ begin
     markanz:=0;          { ggf. /N/U/Z-Nachrichten demarkieren }
   { 01/2000 oh: Nach dem Netcall DatumsbezÅge setzen, damit
     /ØNetzanruf korrekt in der Brettliste auftaucht }
-  if AutoDatumsBezuege then bd_setzen(true);
+  if AutoDatumsBezuege then begin 
+    window(1,1,80,screenlines); {Screenfenster vorher korrigieren!}
+    bd_setzen(true);
+    end; 
   { /oh }
 end;
 
@@ -777,4 +780,3 @@ end;
 
 
 end.
-
