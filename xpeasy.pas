a0b34a9736243276adc04fb1d290772190cef86d
Martin Wodrich <mw@freexp.de>
2000-04-22 14:30:51 +0000

    - EASY-Mode Teil 2 begonnen

---
+++ xpeasy.pas
@@ -0,0 +1,27 @@
+{ --------------------------------------------------------------- }
+{ Dieser Quelltext ist urheberrechtlich geschuetzt.               }
+{ (c) 2000 OpenXP Team (Urversion von Martin Wodrich ,            }
+{ http://www.openxp.de                                            }
+{                                                                 }
+{ Xp-Easy-Konfigurationsmodus (OpenXP)                            }
+{ --------------------------------------------------------------- }
+{ $Id$ }
+
+
+{$I XPDEFINE.INC}
+{$IFDEF BP}
+  {$O+,F+}
+{$ENDIF}
+
+unit XpEasy;
+
+interface
+
+implementation
+{
+  $Log$
+  Revision 1.1  2000/04/22 14:30:51  mw
+
+  - EASY-Mode Teil 2 begonnen
+
+}
