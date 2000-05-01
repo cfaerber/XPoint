3b6225204b0973f3a5bfe851f2adc94837e69b1c
Hinrich Donner <hd@openxp.de>
2000-05-01 09:53:13 +0000

    Curses-Steuerung

---
+++ xpcurses.pas
@@ -0,0 +1,34 @@
+{ --------------------------------------------------------------- }
+{ Dieser Quelltext ist urheberrechtlich geschuetzt.               }
+{ (c) 1991-1999 Peter Mandrella                                   }
+{ (c) 2000 OpenXP Team & Markus K„mmerer, http://www.openxp.de    }
+{ CrossPoint ist eine eingetragene Marke von Peter Mandrella.     }
+{                                                                 }
+{ Die Nutzungsbedingungen fuer diesen Quelltext finden Sie in der }
+{ Datei SLIZENZ.TXT oder auf www.crosspoint.de/srclicense.html.   }
+{ --------------------------------------------------------------- }
+{ $Id$ }
+
+{$I XPDEFINE.INC}
+{$IFDEF BP }
+  {$F+}
+{$ENDIF }
+
+unit xpcurses;
+
+{  ==========================  Interface-Teil  ==========================  }
+
+INTERFACE
+
+
+{ ========================= Implementation-Teil =========================  }
+
+implementation
+
+end.
+{
+  $Log$
+  Revision 1.1  2000/05/01 09:53:13  hd
+  Curses-Steuerung
+
+}
