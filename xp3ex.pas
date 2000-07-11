f7947866238e2dc8720835c4e4c73fd3848fa2bb
Markus Kämmerer <mk@openxp.de>
2000-07-11 08:12:31 +0000

    -- $DAY2, $SUBJECT hinzugefuegt

--- xp3ex.pas
+++ xp3ex.pas
@@ -121,8 +121,9 @@ end;
 
 procedure rpsdate(var s:string);
 begin
-  rps(s,'$DAY','$TAG');
-  rps(s,'$TIME','$UHRZEIT');
+  rps(s, '$DAY2', '$TAG2');
+  rps(s, '$DAY', '$TAG');
+  rps(s, '$TIME', '$UHRZEIT');
   rps(s,'$DATUM',left(date,6)+right(date,2));
   if pos('$DATE',s)>0 then
     rpsdat(s,'$DATE',date);
@@ -724,11 +725,12 @@ begin
           while cpos('/',wempf)>0 do wempf[cpos('/',wempf)]:='.';
           rps(s,'$NEWSGROUP',wempf);
           rpsuser(s,absender,realname);
-          rps(s,'$RNAME', iifs(realname='','',realname+' '));
           rps(s,'$RNAME2', realname);
-          rps(s,'$(RNAME)',iifs(realname='','','('+realname+') '));
+          rps(s,'$RNAME', iifs(realname='','',realname+' '));
           rps(s,'$(RNAME2)',iifs(realname='','','('+realname+')'));
+          rps(s,'$(RNAME)',iifs(realname='','','('+realname+') '));
           rps(s,'$FIDOEMPF',fido_to);
+          rps(s,'$SUBJECT', betreff);
           rps(s,'$BETREFF',betreff);
           rps(s,'$ERSTELLT',fdat(datum));
           if pos('$MSGDATE',UpperCase(s))>0 then
@@ -1060,6 +1062,9 @@ end;
 end.
 {
   $Log$
+  Revision 1.28  2000/07/11 08:12:31  mk
+  -- $DAY2, $SUBJECT hinzugefuegt
+
   Revision 1.27  2000/07/10 19:19:27  mk
   - $DAY, $TIME, $FIRSTNAME hinzugefuegt
 
