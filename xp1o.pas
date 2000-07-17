a3a8726edf94c29dddb7df00deb72b1502ec79ba
Jochen Gehring <jg@openxp.de>
2000-07-17 14:01:42 +0000

    - Bugfixes fuer URL-Erkennung

--- xp1o.pas
+++ xp1o.pas
@@ -119,11 +119,11 @@ begin
       if y=0 then y:=pos('FTP://',ustr(s));                  {oder FTP ?}
       if y=0 then y:=pos('WWW.',ustr(s));                    {oder WWW URL ohne HTTP:? }
       if y<>0 then begin
-        s:=mid(s,y); x:=0;
+        s:=mid(s,y); x:=0; y:=0;
         repeat
           inc (y);                                           {Ende der URL suchen...}
           if (s[y] <= ' ') or (s[y] > '~') or (y=length(s)+1) then x:=y-1;
-          case s[y] of '<', '>', '(', ')', '{', '}', '[', ']' : x:=y-1; end;
+          case s[y] of '<', '>', '(', ')', '{', '}', '[', ']', '"' : x:=y-1; end;
         until x<>0;
         s:=left(s,x);
         end;
@@ -974,6 +974,9 @@ end;
 end.
 {
   $Log$
+  Revision 1.40.2.3  2000/07/17 14:01:42  jg
+  - Bugfixes fuer URL-Erkennung
+
   Revision 1.40.2.2  2000/07/16 22:45:38  mk
   - https bei URL-Erkennung hinzugefuegt
 
