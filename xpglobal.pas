4f8183c64c124440475097c1ea9063d6eb8606d7
Markus Kämmerer <mk@openxp.de>
2000-07-23 22:01:23 +0000

    - Units unter die GPL gestellt

--- xpglobal.pas
+++ xpglobal.pas
@@ -1,10 +1,25 @@
-{ --------------------------------------------------------------- }
-{ Dieser Quelltext ist urheberrechtlich geschuetzt.               }
-{ (c) 2000 OpenXP Team & Markus K„mmerer, http://www.openxp.de    }
-{                                                                 }
-{ Globale Konstanten/Variablen (OpenXP) und Tools                 }
-{ --------------------------------------------------------------- }
-{ $Id$ }
+{  $Id$
+
+   This is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version.
+
+   The software is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this software; see the file gpl.txt. If not, write to the
+   Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   Created on Mai, 3st 2000 by Markus K„mmerer <mk@happyarts.de>
+
+   This software is part of the OpenXP project (www.openxp.de).
+
+   Global definitions, types and constants
+}
 
 unit xpglobal;
 
@@ -120,6 +135,9 @@ begin
 end.
 {
   $Log$
+  Revision 1.37  2000/07/23 22:01:23  mk
+  - Units unter die GPL gestellt
+
   Revision 1.36  2000/07/22 13:26:20  mk
   - variant-typ fuer vp
 
