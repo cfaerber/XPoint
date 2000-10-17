c54a1d2bd7340fa7010b3f767630c56ee279e37a
Frank Ellert <fe@openxp.de>
2000-10-17 14:52:22 +0000

    make.bat in build.bat umbenannt um Verwechselungen mit GNU make zu vermeiden.

---
+++ build.bat
@@ -0,0 +1,42 @@
+@echo off
+rem OpenXP "make" Batchdatei: Kompiliert alle fuer XP relevanten
+rem Programme und Resourcen
+rem Compiler ggf. als Kommandozeilenparameter uebergeben.
+if "%comp%"=="" set comp=%1
+if "%comp%"=="" set comp=ppc386
+%comp% xp
+if errorlevel 1 goto Fehler
+%comp% objcom/objcom
+if errorlevel 1 goto Fehler
+%comp% xp-fm
+if errorlevel 1 goto Fehler
+%comp% uuz
+if errorlevel 1 goto Fehler
+%comp% ndiff
+if errorlevel 1 goto Fehler
+%comp% rc
+if errorlevel 1 goto Fehler
+%comp% ihs
+if errorlevel 1 goto Fehler
+rc xp-d.rq
+if errorlevel 1 goto Fehler
+rc xp-e.rq
+if errorlevel 1 goto Fehler
+rc xpfm-d.rq
+if errorlevel 1 goto Fehler
+rc xpfm-e.rq
+if errorlevel 1 goto Fehler
+ihs doc\xp
+if errorlevel 1 goto Fehler
+ihs doc\xp-e
+if errorlevel 1 goto Fehler
+
+:Ende
+echo Fertig.
+goto Ex
+
+:Fehler
+echo Es ist ein Fehler aufgetreten.
+goto Ex
+
+:Ex
