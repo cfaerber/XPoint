ad1cf36afe0f9bd5db3b63891e572f79f8d9eaf5
Markus Kämmerer <mk@openxp.de>
2000-12-31 11:52:10 +0000

    JG:- MsgId-Suche mit mehreren Strings

--- xp4o.pas
+++ xp4o.pas
@@ -119,9 +119,9 @@ end;
 function mid_suchoption(var s:string):boolean;
 begin
   setfieldenable(mid_options,s='J');
-  setfieldenable(mid_bretter,s='J');
+  if aktdispmode <> 11 then setfieldenable(mid_bretter,s='J');
   mid_suchoption:=true;
-  end;
+end;
 
 { Suchfeld:  '' (Volltext), '*' (Umiversal), 'Betreff', 'Absender', 'MsgID' }
 
@@ -767,10 +767,15 @@ begin
       suche:=false;
       if not brk then begin
         markanz:=0;
-        n:=GetBezug(suchstring);
-        if n<>0 then begin
-          dbGo(mbase,n);
-          MsgAddmark;
+        check_seekmode;
+        for i:=0 to suchanz-1 do
+        begin
+          seek:=copy(suchstring,seekstart[i],seeklen[i]);
+          n:=GetBezug(seek);
+          if n<>0 then begin
+            dbGo(mbase,n);
+            MsgAddmark;
+            end;
           end;
         end;
       end
@@ -2421,6 +2426,9 @@ end;
 end.
 {
   $Log$
+  Revision 1.87  2000/12/31 11:52:10  mk
+  JG:- MsgId-Suche mit mehreren Strings
+
   Revision 1.86  2000/12/30 18:45:17  mo
   -Spezialsuche in markierten Bretter auch aus der Nachrichten/User Übersicht
 
