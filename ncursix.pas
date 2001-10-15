49c0176f611ed89da2dac9828223586fb5e8eb21
Matthias Leonhardt <ml@openxp.de>
2001-10-15 09:39:10 +0000

    - copyright

--- ncursix.pas
+++ ncursix.pas
@@ -12,6 +12,24 @@
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
+    Kylix compatibility-changes
+    Copyright (C) 2000-2001 OpenXP team (www.openxp.de) & Matthias Leonhardt
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+
  **********************************************************************}
 
 {
@@ -1745,6 +1763,9 @@ end;
 
 {
   $Log$
+  Revision 1.6  2001/10/15 09:39:10  ml
+  - copyright
+
   Revision 1.5  2001/10/15 09:32:42  ml
   - now using packed records
 
