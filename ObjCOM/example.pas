964763585a0618b77be9d75cb5b4e4f6dde6e59f
Malte Kiesel <ma@openxp.de>
2002-01-04 19:54:18 +0000

    - compiles again

--- ObjCOM/example.pas
+++ ObjCOM/example.pas
@@ -6,7 +6,7 @@ program Example;
 
 uses ObjCOM,CRT;
 
-var CommObj: tpCommObj;
+var CommObj: tCommStream;
     CommOpen,Pass,ConvInv: Boolean;
     C: Char;
     SInit,NInit: String;
@@ -18,7 +18,8 @@ begin
   WriteLn('Default init string: ',SInit);
   Write('Enter init string: '); ReadLn(NInit);
   if NInit<>'' then SInit:=NInit;
-  if not CommInit(SInit,CommObj)then begin WriteLn('Error opening port: ',ErrorStr); Halt(1)end;
+  CommObj:=CommInit(SInit);
+  if not assigned(CommObj)then begin WriteLn('Error opening port: ',ErrorStr); Halt(1)end;
 
   WriteLn('Special functions (press # to use):');
   WriteLn('Q: Quit  S/P: SendStr/PauseCom test  D: Display carrier  C: Display<#32');
@@ -30,17 +31,17 @@ begin
       c:=ReadKey;
       if c='#' then case UpCase(ReadKey)of
         'Q': pass:=True;
-        'S': begin WriteLn('SendStr result: ',CommObj^.SendString('ATZ'+#13,True)); Write(CommObj^.ErrorStr)end;
+        'S': begin WriteLn('SendStr result: ',CommObj.SendString('ATZ'+#13,True)); Write(CommObj.ErrorStr)end;
         'P': begin CommOpen:=not CommOpen; WriteLn('CommOpen: ',CommOpen);
-                   if CommOpen then CommObj^.ResumeCom(False) else CommObj^.PauseCom(True)end;
+                   if CommOpen then CommObj.ResumeCom(False) else CommObj.PauseCom(True)end;
         'C': begin ConvInv:=not ConvInv; WriteLn('Print ASCII values: ',ConvInv)end;
-        'D': WriteLn('Carrier: ',CommObj^.Carrier);
-      end else CommObj^.SendChar(c);
+        'D': WriteLn('Carrier: ',CommObj.Carrier);
+      end else CommObj.SendChar(c);
      end;
     I:=500;
-    while CommObj^.CharAvail and(I>1)do begin
+    while CommObj.CharAvail and(I>1)do begin
       Dec(I);
-      C:=CommObj^.GetChar;
+      C:=CommObj.GetChar;
       if C>=#32 then Write(C)
       else if not ConvInv then Write(C)
       else Write('#',Ord(C));
@@ -48,7 +49,7 @@ begin
     Delay(10);
   until Pass;
 
-  CommObj^.Close; Dispose(CommObj, Done);
+  CommObj.Destroy;
 end.
 
 
