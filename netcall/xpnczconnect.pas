19c70235f91533f8733fd8eb6c61d573abd29545
Malte Kiesel <ma@openxp.de>
2002-11-01 14:17:05 +0000

    - reimplemented Janus+ switch, should solve problems with non-standard   servers

--- netcall/xpnczconnect.pas
+++ netcall/xpnczconnect.pas
@@ -54,6 +54,10 @@ procedure ProcessIncomingFiles(FilesToProcess: TStringList;
   function isCompressedZCPacket(s: string): Boolean;
   var i,p: Integer;
   begin
+    if(not boxpar^.JanusPlus)then begin
+      result:=true;
+      exit;
+      end;
     s:=ExtractFilename(s);
     p:=cPos('.',s);
     result:=(UpperCase(Copy(s,2,1))='P')and(p<>0); { Janus+ }
@@ -251,6 +255,10 @@ end.
 
 {
   $Log$
+  Revision 1.14  2002/11/01 14:17:06  ma
+  - reimplemented Janus+ switch, should solve problems with non-standard
+    servers
+
   Revision 1.13  2002/08/03 16:31:41  mk
   - fixed unsendt-handling in client-mode
 
--- xpconfigedit.pas
+++ xpconfigedit.pas
@@ -94,7 +94,6 @@ function  testsysname(var s:string):boolean;
 function  testlogfile(var s:string):boolean;
 function  TestAKAservers(var s:string):boolean;
 function  testZCpointname(var s:string):boolean;
-function  JanusSwitch(var s:string):boolean;
 
 function  PPPClientPathTest(var s:string):boolean;
 function  PPPClientTest(var s:string):boolean;
@@ -653,32 +652,6 @@ begin
     rfehler1(930,us);    { 'Warnung: Ungltige Zeichen im Pointname: %s' }
   testZCpointname:=true;  { (us=''); }
 end;
-
-{$IFDEF FPC }
-  {$HINTS OFF }
-{$ENDIF }
-
-function JanusSwitch(var s:string):boolean;
-var x,y, anz,i : integer;
-    t     : taste;
-begin
-  JanusSwitch:=true;
-  if LowerCase(getfield(downprotnr))='zmodem' then exit;
-  anz:=res2anz(932);
-  msgbox(63,anz+5,_hinweis_,x,y);
-  for i:=1 to anz do
-    wrt(x+3,y+1+i,getres2(932,i));
-  wrt(x+3,y+3+anz,getres(12));    { 'Taste drcken ...' }
-  errsound;
-  get(t,curon);
-  closebox;
-end;
-
-{$IFDEF FPC }
-  {$HINTS ON }
-{$ENDIF }
-
-
 
 { Typ :  1=Boxen, 2=Gruppen, 3=Systeme, 4=Kurznamen, 5=MIME-Typen }
 { edit:  true=editieren, false=nur ausw„hlen                      }
@@ -2541,6 +2514,10 @@ end;
 
 {
   $Log$
+  Revision 1.51  2002/11/01 14:17:05  ma
+  - reimplemented Janus+ switch, should solve problems with non-standard
+    servers
+
   Revision 1.50  2002/07/31 19:26:21  ma
   - user=>email db field code synchronized with v3.8
     (does not need re-entering email address when upgrading from old
