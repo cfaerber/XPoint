711eb99076cece98552090a3f98b3981d6e9f6f4
Markus Kämmerer <mk@openxp.de>
2003-05-10 15:44:09 +0000

    - show two more lines when listing messages after PgDown

--- lister.pas
+++ lister.pas
@@ -934,7 +934,7 @@ begin // Show
         if stat.allpgdn then
         begin
           if Lines.Count-DispLines > FirstLine then
-            FirstLine := Min(FirstLine + DispLines, Max(Lines.Count, 0));
+            FirstLine := Min(FirstLine + DispLines - 2, Max(Lines.Count, 0));
         end else
           FirstLine := Min(FirstLine + DispLines, Max(Lines.Count - DispLines, 0));
         FSelLine := Min(FSelLine + DispLines, Max(Lines.Count - 1, 0));
@@ -1139,6 +1139,9 @@ initialization
 finalization
 {
   $Log$
+  Revision 1.69.2.3  2003/05/10 15:44:09  mk
+  - show two more lines when listing messages after PgDown
+
   Revision 1.69.2.2  2003/05/01 10:53:45  mk
   - restored correct page down handling, when stat.allpgdn = true
 
