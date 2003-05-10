b7b286d68843b58f8de52b1a83a76242b9d8327c
Markus Kämmerer <mk@openxp.de>
2003-05-10 16:34:30 +0000

    - fixed highlighting of lines in lister

--- xp0.pas
+++ xp0.pas
@@ -1209,7 +1209,7 @@ var    bb_brettname,bb_kommentar,bb_ldatum,bb_flags,bb_pollbox,bb_haltezeit,
 { in attrbuf. beschrieben werden sie in xp1.MakeListDisplay, gelesen in Winxp.consolewrite }
 
   charbuf     : shortstring;                 { Nicht zu klein :-) }
-  attrbuf     : array [1..sizeof(shortstring)] of smallword;
+  attrbuf     : array [0..sizeof(shortstring)] of smallword;
 
   // Speichert alle Zeilen in der Konfiguration, die nicht
   // erkannt und ausgewertet wurden, siehe xp2cfg.inc
@@ -1225,6 +1225,9 @@ implementation
 
 {
   $Log$
+  Revision 1.178  2003/05/10 16:34:30  mk
+  - fixed highlighting of lines in lister
+
   Revision 1.177  2003/05/01 09:52:28  mk
   - added IMAP support
 
