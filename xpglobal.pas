5aa31660db023adf5bbd9fe733b1d120a7b2d9c2
Markus Kämmerer <mk@openxp.de>
2003-06-22 11:09:17 +0000

    - version 3.8.11 wegen kaputten internen news

--- xpglobal.pas
+++ xpglobal.pas
@@ -31,7 +31,7 @@ interface
 // verstr+pformstr+betastr is readable
 
 const
-  verstr      = 'v3.8.10';      { Versionnr. - steht nur an dieser Stelle }
+  verstr      = 'v3.8.11';      { Versionnr. - steht nur an dieser Stelle }
   betastr     = ' beta';        { ' ' oder ' beta' }
 
   {$IFDEF Win32 }
@@ -192,6 +192,9 @@ begin
 
 {
   $Log$
+  Revision 1.63.2.27  2003/06/22 11:09:17  mk
+  - version 3.8.11 wegen kaputten internen news
+
   Revision 1.63.2.26  2003/06/01 15:42:54  mk
   - version 3.8.10
 
