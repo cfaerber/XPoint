eb9f78e4e55b432f01a182036823a6e7de20bb10
Markus Kämmerer <mk@openxp.de>
2003-09-05 17:03:42 +0000

    - fixed bug with to large ArticleIndex

--- netcall/xpncnntp.pas
+++ netcall/xpncnntp.pas
@@ -275,7 +275,7 @@ var
    end;
 
 const
-  MaxMessagecount = 5; // Maximum number of Messages in Pipeline
+  MaxMessagecount = 15; // Maximum number of Messages in Pipeline
 var
   NNTP          : TNNTP;                { Socket }
   POWindow      : TProgressOutputWindow;{ ProgressOutput }
@@ -283,6 +283,7 @@ var
   RCFilename    : String;
   MIDFilename   : String;
   oArticle      : integer;
+  ArticleCount  : Integer;
   s, MsgId: String;
 begin
   { POWindow erstellen }
@@ -364,8 +365,9 @@ begin
         begin
           POWindow.WriteFmt(mcVerbose,res_getposting,[ArticleIndex-oArticle,NNTP.LastMessage-oArticle]);
 
-          NNTP.GetMessage(ArticleIndex, Min(MaxMessageCount, NNTP.LastMessage - ArticleIndex + 1), List, HeaderOnly);
-          Inc(ArticleIndex, MaxMessageCount);
+          ArticleCount := Min(MaxMessageCount, NNTP.LastMessage - ArticleIndex + 1);
+          NNTP.GetMessage(ArticleIndex, ArticleCount, List, HeaderOnly);
+          Inc(ArticleIndex, ArticleCount);
 
           if List.Count > 10000 then
             SaveNews(true);
@@ -422,6 +424,9 @@ end;
 
 {
         $Log$
+        Revision 1.52  2003/09/05 17:03:42  mk
+        - fixed bug with to large ArticleIndex
+
         Revision 1.51  2003/09/04 23:11:36  mk
         - minimal fix for new nnntp
         - get only five messages at once in the pipeline
