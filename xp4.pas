c6f59c973a76fc870a950a1ab8a35cec91e10c99
Markus Kämmerer <mk@openxp.de>
2006-04-17 17:17:51 +0000

    - fixed db error in ToggleMessageIDRequest, when box was not found
    git-svn-id: https://svn.openxp.de/openxp/branches/branch_4_10@7263 65356fb5-50e6-0310-a06b-fbc0adf8d39c

--- xp4.pas
+++ xp4.pas
@@ -2349,6 +2349,8 @@ var t,lastt: taste;
 
         Box := dbReadNStr(mbase, mb_brett);
         dbSeek(bbase,BiIntnr,copy(Box,2,4));
+        if dbEOF(bbase) or dbBOF(bbase) then        
+          continue;
         Box := dbReadNStr(bbase, bb_pollbox);      { Pollbox des Brettes     }
         // Hilfe nachtragen
 
