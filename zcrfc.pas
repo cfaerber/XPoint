4f5b5a3feabf8a542aeeb4bc209ad308f0975dd6
Hans-Jürgen Tänzer <hans-juergen.taenzer@t-online.de>
2006-11-11 19:35:44 +0000

    remove all invalid characters from decoded header lines (0d/0a/00/1a)(uuz -uz)
    git-svn-id: https://svn.openxp.de/openxp/branches/branch_4_10@7308 65356fb5-50e6-0310-a06b-fbc0adf8d39c

--- zcrfc.pas
+++ zcrfc.pas
@@ -230,37 +230,6 @@ end;
 
 function unbatch(s: string): boolean; forward;
 
-{ HJT 12.01.2006                                  }
-{ Aus dekodierten RFC-Headerzeilen alle Zeichen   }
-{ entfernen, die dazu fuehren wuerden, dass ein   }
-{ mehrzeiliger ZConnect-Header entsteht.          }
-{ 'in the wild' wurden solche Header speziell bei }
-{ kodierten X-Face-Headern gesichtet.             }
-{ Auch der 'ZPR' kommt mit den gefoldeten Headern }
-{ nicht klar.                                     }
-function remove_crlf(const s : string): string;
-var 
-    len : integer; 
-    i   : integer;
-    z   : integer;
-begin
-    len := length(s);
-    SetLength(result, len); 
-    z := 0;
-    for i := 1 to len do begin
-        if (Ord(s[i]) <> $0d) and (Ord(s[i]) <> $0a) then begin
-            z := z + 1;
-            result[z] := s[i];
-        end
-        else begin
-            Debug.DebugLog('zcrfc', 'remove_crlf, removed '
-                           +Hex(Ord(s[i]),2)+' from header: '
-                           +'<'+s+'>', DLInform);
-        end;
-    end;
-    SetLength(result, z);
-end;
-
 constructor TUUZ.create;
 var
   t: Text;
@@ -2097,11 +2066,7 @@ begin
 
     for i := 0 to ULine.Count-1 do
     begin
-      s := ULine[i];
-      s := RFC2047_Decode(s,csCP437);
-      { HJT 12.01.2006: gegebenenfalls cr/lf entfernen }
-      { ULine[i] := s; }
-      ULine[i] := remove_crlf(s);
+      ULine[i] := RFC2047_Decode(ULine[i],csCP437);
     end;
 
     if pm_reply then
