69f040ce3c14d41b99c4084978cbbe06cee8c552
Hans-Jürgen Tänzer <hans-juergen.taenzer@t-online.de>
2007-06-16 06:12:51 +0000

    delete first of dublicate BEZ while storing references for 'bezbaum'
    git-svn-id: https://svn.openxp.de/openxp/branches/branch_4_10@7342 65356fb5-50e6-0310-a06b-fbc0adf8d39c

--- xpmakeheader.pas
+++ xpmakeheader.pas
@@ -61,6 +61,7 @@ var i,res : integer;
     bufsize : Integer;
     bufanz  : Integer;   { gelesene Bytes im Puffer }
     tc      : char;   { 1. Trennzeichen hinter ':' }
+    idummy  : Integer;
 
   procedure ReadBuf;
   begin
@@ -418,8 +419,21 @@ begin
             if id = 'KOM'    then val(line,komlen,res) else
             if id = 'KOP'    then hd.Kopien.Add(line) else
             if id = 'BEZ'    then begin
-                                    if (Line<>'')and(References.IndexOf(Line)=-1) then
-                                      References.Add(Line)end else
+                                  if (Line<>'') then
+                                  begin
+                                    idummy := References.IndexOf(Line);
+                                    if idummy <> -1 then
+                                    begin
+                                      Debug.DebugLog('xpmakeheader.pas','makeheader,'
+                                                     +'deleting first of dublicate BEZ:<'
+                                                     +References[idummy]+'>'
+                                                     +', first index: '+IntToStr(idummy)
+                                                     , DLDebug);
+                                      References.Delete(idummy);
+                                    end;
+                                    References.Add(Line);
+                                  end
+                             end else
             if id = 'MAILER' then programm := line else
             if id = 'ORG'    then organisation := line else
             if id = 'OEM'    then hd.Oem.Add(Line) else
